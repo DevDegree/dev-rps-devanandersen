e5ce3c9f045411238b3a958e78c3142d3870409d
devanandersen <devan.andersen@shopify.com>
2019-09-12 16:16:15 -0400

    Adding modifications for part 4, updating program to run until one player wins best out of 3 matches.

--- rock_paper_scissors.rb
+++ rock_paper_scissors.rb
@@ -1,16 +1,20 @@
 puts "Rock Paper Scissors"
-def start_game()
-  print "Player 1 - Enter your selection: " 
-  player1_selection = gets.chomp.downcase
-  print "Player 2 - Enter your selection: "
-  player2_selection = gets.chomp.downcase
-  value = rock_paper_scissors(player1_selection, player2_selection)
-  score_keep(value)
-end
 
-def score_keep(string)
-  if (string === "Tie!" || string === "Cheating!") then
-    start_game()
+def start_game()
+  player1_score = 0
+  player2_score = 0
+  until (player1_score - player2_score  === 2 || player2_score - player1_score === 2) do
+    print "Player 1 - Enter your selection: " 
+    player1_selection = gets.chomp.downcase
+    print "Player 2 - Enter your selection: "
+    player2_selection = gets.chomp.downcase
+
+    value = rock_paper_scissors(player1_selection, player2_selection)
+    if (value === "player 1") then
+      player1_score += 1
+    elsif (value === "player2") then
+      player2_score += 1
+    end
   end
 end
 
@@ -25,9 +29,11 @@ def rock_paper_scissors(player1, player2)
     when "paper"
       puts "Paper covers rock."
       puts "Player 2 wins."
+      value = "player 2"
     when "scissors"
       puts "Rock crushes scissors."
       puts "Player 1 wins."
+      value = "player 1"
     else
       puts "No cheaters! Only Rock, Paper, or Scissors are allowed."
     end
@@ -36,12 +42,14 @@ def rock_paper_scissors(player1, player2)
     when "rock"
       puts "Paper covers rock."
       puts "Player 1 wins."
+      value = "player 1"
     when "paper"
       puts "Tie!"
       value = "Tie!"
     when "scissors"
       puts "Scissors cuts paper."
       puts "Player 2 wins."
+      value = "player 2"
     else
       puts "No cheaters! Only Rock, Paper, or Scissors are allowed."
       value = "Cheaters!"
@@ -51,9 +59,11 @@ def rock_paper_scissors(player1, player2)
     when "rock"
       puts "Rock crushes scissors."
       puts "Player 2 wins."
+      value = "player 2"
     when "paper"
       puts "Scissors cuts paper."
       puts "Player 1 wins."
+      value = "player 1"
     when "scissors"
       puts "Tie!"
       value= "Tie!"
