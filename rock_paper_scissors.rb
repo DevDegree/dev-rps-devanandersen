0a64ea3886bf43c80975b29a8a75ce2b64275b2c
devanandersen <devan.andersen@shopify.com>
2019-09-12 15:43:59 -0400

    Add file modifications for part 3, re-prompting if there is a tie or cheating

--- rock_paper_scissors.rb
+++ rock_paper_scissors.rb
@@ -1,55 +1,71 @@
 puts "Rock Paper Scissors"
-print "Player 1 - Enter your selection: " 
-player1_selection = gets.chomp.downcase
-print "Player 2 - Enter your selection: "
-player2_selection = gets.chomp.downcase
+def start_game()
+  print "Player 1 - Enter your selection: " 
+  player1_selection = gets.chomp.downcase
+  print "Player 2 - Enter your selection: "
+  player2_selection = gets.chomp.downcase
+  value = rock_paper_scissors(player1_selection, player2_selection)
+  score_keep(value)
+end
 
+def score_keep(string)
+  if (string === "Tie!" || string === "Cheating!") then
+    start_game()
+  end
+end
 
 def rock_paper_scissors(player1, player2) 
+  value = ''
   case player1
   when "rock"
     case player2
     when "rock"
-      print "Tie!"
+      puts "Tie!"
+      value = "Tie!"
     when "paper"
       puts "Paper covers rock."
-      print "Player 2 wins."
+      puts "Player 2 wins."
     when "scissors"
       puts "Rock crushes scissors."
-      print "Player 1 wins."
+      puts "Player 1 wins."
     else
-      print "No cheaters! Only Rock, Paper, or Scissors are allowed."
+      puts "No cheaters! Only Rock, Paper, or Scissors are allowed."
     end
   when "paper"
     case player2
     when "rock"
       puts "Paper covers rock."
-      print "Player 1 wins."
+      puts "Player 1 wins."
     when "paper"
-      print "Tie!"
+      puts "Tie!"
+      value = "Tie!"
     when "scissors"
       puts "Scissors cuts paper."
-      print "Player 2 wins."
+      puts "Player 2 wins."
     else
-      print "No cheaters! Only Rock, Paper, or Scissors are allowed."
+      puts "No cheaters! Only Rock, Paper, or Scissors are allowed."
+      value = "Cheaters!"
     end
   when "scissors"
     case player2
     when "rock"
       puts "Rock crushes scissors."
-      print "Player 2 wins."
+      puts "Player 2 wins."
     when "paper"
       puts "Scissors cuts paper."
-      print "Player 1 wins."
+      puts "Player 1 wins."
     when "scissors"
-      print "Tie!"
+      puts "Tie!"
+      value= "Tie!"
     else
-      print "No cheaters! Only Rock, Paper, or Scissors are allowed."
+      puts "No cheaters! Only Rock, Paper, or Scissors are allowed."
+      value = "Cheating!"
     end
   else
-    print "No cheaters! Only Rock, Paper, or Scissors are allowed."
+    puts "No cheaters! Only Rock, Paper, or Scissors are allowed."
+    value = "Cheating!"
   end
+  return value
 end
 
-rock_paper_scissors(player1_selection, player2_selection)
-
+start_game()
